f38a4861b79a165ef404b84ad314ba0b03bafa70
HoRalph <ralph.ho94@gmail.com>
2025-01-12 00:05:32 -0500

    start project

---
+++ Console.Drinks/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http.Headers;
+
+public class Program
+{
+    public static Main(string[] args)
+    {
+        using HttpClient client = new();
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+        client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+       await ProcessRepositoriesAsync(client);
+    }
+    
+    
+await ProcessRepositoriesAsync();
+
+static async Task ProcessRepositoriesAsync(HttpClient client)
+{
+}
+ 
+
+}
