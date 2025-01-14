509e2316d24ef54d844efd156d63a9ad67106d54
HoRalph <ralph.ho94@gmail.com>
2025-01-13 21:53:51 -0500

    HTTPS request tutorial

--- Console.Drinks/Program.cs
+++ Console.Drinks/Program.cs
@@ -1,23 +1,33 @@
-﻿using System.Net.Http.Headers;
+﻿using System.ComponentModel;
+using System.Net.Http.Headers;
+using System.Text.Json;
 
-public class Program
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+var repositories = await ProcessRepositoriesAsync(client);
+
+foreach (var repo in repositories)
 {
-    public static Main(string[] args)
-    {
-        using HttpClient client = new();
-        client.DefaultRequestHeaders.Accept.Clear();
-        client.DefaultRequestHeaders.Accept.Add(
-            new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
-        client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
-       await ProcessRepositoriesAsync(client);
-    }
-    
-    
-await ProcessRepositoriesAsync();
-
-static async Task ProcessRepositoriesAsync(HttpClient client)
-{
+    Console.WriteLine($"Name: {repo.Name}");
+    Console.WriteLine($"Homepage: {repo.Homepage}");
+    Console.WriteLine($"GitHub: {repo.GitHubHomeUrl}");
+    Console.WriteLine($"Description: {repo.Description}");
+    Console.WriteLine($"Watchers: {repo.Watchers:#,0}");
+    Console.WriteLine($"Last push: {repo.LastPush}");
+    Console.WriteLine();
 }
- 
 
-}
+static async Task<List<Repository>> ProcessRepositoriesAsync(HttpClient client)
+ {
+     await using Stream stream = await client.GetStreamAsync(
+         "https://api.github.com/orgs/dotnet/repos");
+    var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(stream);
+    return repositories ?? new();
+
+ }
+
+//https://learn.microsoft.com/en-us/dotnet/csharp/tutorials/console-webapiclient
